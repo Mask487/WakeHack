fc076da3762adfcfb59f714045ec145c37a93f53
WholianS <ncpantherspride@gmail.com>
2019-03-30 14:12:10 -0400

    changed from 2.6 to 3.6'

--- App.py
+++ App.py
@@ -3,18 +3,18 @@ import EventHandler
 import os
 
 
-startMessage = "Welcome To The App You Dumb Bitch\n1 -- View Events\n2 -- Add Event\n3 -- Remove Event\n4 -- End\n"
-input = 0
+startMessage = "Welcome To Wake Scheduler\n1 -- View Events\n2 -- Add Event\n3 -- Remove Event\n4 -- End\n"
+decision = 0
 events = EventHandler.getAllEvents()
 
-while int(input) != int(4):
+while decision != int(4):
     print(startMessage)
-    input = raw_input("Please Enter What You Would Like To Do: ")
+    decision = int(input("Please Enter What You Would Like To Do: "))
     #os.system('cls')
-    if int(input) == int(1):
+    if decision == int(1):
         if events.__len__() == 0:
             print("No Events")
-            input = 0
+            decision = 0
         else:
             for e in events:
                 print("Event")
@@ -23,35 +23,35 @@ while int(input) != int(4):
                 print("Description: " + e.getDescription())
                 print("Priority: " + e.getPriority())
                 print("---------------------------------\n")
-            input = 0
-    elif int(input) == int(2):
-        date = raw_input("Please Enter Date(Format M D Y): ")
-        time = raw_input("Please Enter Time: ")
-        desc = raw_input("Please Enter Description: ")
-        priority = raw_input("Please Enter Priority: ")
-        rep = raw_input("Please Enter Repetition: ")
+            decision = 0
+    elif decision == int(2):
+        date = input("Please Enter Date(Format M D Y): ")
+        time = input("Please Enter Time: ")
+        desc = input("Please Enter Description: ")
+        priority = input("Please Enter Priority: ")
+        rep = input("Please Enter Repetition: ")
 
         event = EventOBJ.Event(date, time, desc, priority, rep)
         events = EventHandler.getAllEvents()
 
         if events.__contains__(event):
             print("Event Already Exists")
-            input = 0
+            decision = 0
         else:
             EventHandler.addEvent(event)
-            input = 0
-    elif int(input) == int(3):
+            decision = 0
+    elif decision == int(3):
         print("Please Enter The Following Information Of The Event You Want Removed.")
-        date = raw_input("Please Enter Date(Format M D Y): ")
-        time = raw_input("Please Enter Time: ")
-        desc = raw_input("Please Enter Description: ")
-        priority = raw_input("Please Enter Priority: ")
-        rep = raw_input("Please Enter Repetition: ")
+        date = input("Please Enter Date(Format M D Y): ")
+        time = input("Please Enter Time: ")
+        desc = input("Please Enter Description: ")
+        priority = input("Please Enter Priority: ")
+        rep = input("Please Enter Repetition: ")
         event = EventOBJ.Event(date, time, desc, priority, rep)
         EventHandler.removeEvent(event)
 
-        input = 0
-    elif int(input) == 4:
+        decision = 0
+    elif decision == int(4):
         print("Exiting...")
     else:
-        input = raw_input("You Dumb Fucker, You Had Once Job. Put In A Damn Number 1-4:")
+        decision = input("You Dumb Fucker, You Had Once Job. Put In A Damn Number 1-4:")
--- Test.py
+++ Test.py
@@ -13,11 +13,11 @@ desc = ET.SubElement(event, "desc")
 priority = ET.SubElement(event, "priority")
 rep = ET.SubElement(event, "rep")
 
-date1 = raw_input("What is your date? ")
-time1 = raw_input("\nWhat is your time? ")
-desc1 = raw_input("\nWhat is your desc? ")
-priority1 = raw_input("\nWhat is your priority? ")
-rep1 = raw_input("\nWhat is your rep? ")
+date1 = input("What is your date? ")
+time1 = input("\nWhat is your time? ")
+desc1 = input("\nWhat is your desc? ")
+priority1 = input("\nWhat is your priority? ")
+rep1 = input("\nWhat is your rep? ")
 
 date.text = date1
 time.text = time1
